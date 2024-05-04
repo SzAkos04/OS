1f488caf996fb62018a333c9ba2877fb3eb6a9bb
SzAkos04 <arisenwall1124@gmail.com>
2024-05-04 17:41:18 +0200

    Added bugs

--- src/bootloader.asm
+++ src/bootloader.asm
@@ -1,24 +1,53 @@
 [org 0x7c00]
+KERNEL_OFFSET equ 0x1000
 
-global _start
-
-_start:
     mov [BOOT_DRIVE], dl
 
-    mov bp, 0x8000
+    mov bp, 0x9000
     mov sp, bp
 
-    mov bx, 0x9000
-    mov dh, 5
-    mov dl, [BOOT_DRIVE]
-    call disk_load
+    mov bx, MSG_REAL_MODE
+    call print_str
+
+    call load_kernel
+
+    call switch_to_pm
 
     jmp $
 
-%include "src/io.asm"
+%include "src/print_str.asm"
 %include "src/disk.asm"
+%include "src/gdt.asm"
+%include "src/print_str_pm.asm"
+%include "src/switch_to_pm.asm"
+
+[bits 16]
+
+load_kernel:
+    mov bx, MSG_LOAD_KERNEL
+    call print_str
+
+    mov bx, KERNEL_OFFSET
+    mov dh, 15
+    mov dl, [BOOT_DRIVE]
+    call disk_load
+
+    ret
+
+[bits 32]
+
+BEGIN_PM:
+    mov ebx, MSG_PROT_MODE
+    call print_str_pm
+
+    call KERNEL_OFFSET
+
+    jmp $
 
 BOOT_DRIVE db 0
+MSG_REAL_MODE db "Started in 16-bit Real Mode", 0x0d, 0x0a, 0
+MSG_PROT_MODE db "Successfully landed in 32-bit Protected Mode", 0
+MSG_LOAD_KERNEL db "Loading kernel into memory", 0x0d, 0x0a, 0
 
 times 510-($-$$) db 0
 dw 0xaa55
--- src/print_hex.asm
+++ src/print_hex.asm
@@ -1,19 +1,4 @@
-print_str:
-    pusha
-    mov ah, 0x0e
-.repeat:
-    mov al, [bx]
-    cmp al, 0
-    je .done
-    int 0x10
-    inc bx
-    jmp .repeat
-.done:
-    popa
-    ret
-
 print_hex:
-    ; manipulate chars at HEX_OUT to reflect DX
     mov cx, dx
     and cx, 0xf000
     shr cx, 12
@@ -38,7 +23,7 @@ print_hex:
     mov [HEX_OUT + 5], cx
 
     mov bx, HEX_OUT
-    call print_str
+    call print_string
     mov byte [HEX_OUT + 2], '0'
     mov byte [HEX_OUT + 3], '0'
     mov byte [HEX_OUT + 4], '0'
