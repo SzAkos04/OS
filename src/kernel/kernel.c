fd82566f048663f7a7930bbd72bbe136ff901be6
SzAkos04 <arisenwall1124@gmail.com>
2024-05-05 00:19:44 +0200

    Fixed forgotten files

--- src/kernel/kernel.c
+++ src/kernel/kernel.c
@@ -1,8 +1,4 @@
 #include "screen.h"
 #include "utils.h"
 
-#define VIDEO_ADDRESS 0xa0000
-#define SCREEN_WIDTH 320
-#define SCREEN_SIZE 200
-
 void _main_c() { putpixel(50, 50, 0x0f); }
--- src/kernel/screen.c
+++ src/kernel/screen.c
@@ -2,7 +2,7 @@
 
 #define VIDEO_ADDRESS 0xa0000
 #define SCREEN_WIDTH 320
-#define SCREEN_SIZE 200
+#define SCREEN_HEIGHT 200
 
 void putpixel(int pos_x, int pos_y, u8 VGA_COLOR) {
     u8 *location = (u8 *)VIDEO_ADDRESS + SCREEN_WIDTH * pos_y + pos_x;
