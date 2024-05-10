5fbcb0a492fd68d556a3b5536294c31dbf833a0c
SzAkos04 <arisenwall1124@gmail.com>
2024-05-10 12:40:42 +0200

    Added bugs

--- src/kernel/kernel.c
+++ src/kernel/kernel.c
@@ -5,8 +5,16 @@
 void _main_c() {
     screen_init();
     clear_screen();
+    draw_rect(50, 50, 100, 100, COLOR(0x00, 0x00, 0xff));
 
-    for (int i = 0; i < 255; i++) {
-        draw_line(0, i, SCREEN_WIDTH, i, i);
-    }
+    draw_line(50, 50, 75, 75, COLOR(0x00, 0xff, 0x00));
+    draw_line(50, 100, 75, 125, COLOR(0x00, 0xff, 0x00));
+    draw_line(100, 50, 125, 75, COLOR(0xff, 0xff, 0x00));
+    draw_line(100, 100, 125, 125, COLOR(0xff, 0xff, 0x00));
+
+    draw_rect(75, 75, 125, 125, COLOR(0xff, 0x00, 0x00));
+    // FIXME: for some reason this doesn't print anything, but if I comment out
+    // a `draw_line()` function, it works, looks like it has a limit on how much
+    // it can put on the screen
+    print_string("CUBE", 75, 30, COLOR(0xff, 0xff, 0xff));
 }
--- src/kernel/screen.h
+++ src/kernel/screen.h
@@ -8,18 +8,6 @@
 #define COLOR(_r, _g, _b)                                                      \
     ((u8)((((_r) & 0x7) << 5) | (((_g) & 0x7) << 2) | (((_b) & 0x3) << 0)))
 
-#define COLOR_R(_index) (((_index) >> 5) & 0x7)
-#define COLOR_G(_index) (((_index) >> 2) & 0x7)
-#define COLOR_B(_index) (((_index) >> 0) & 0x3)
-
-#define COLOR_ADD(_index, _d)                                                  \
-    __extension__({                                                            \
-        __typeof__(_index) _c = (_index);                                      \
-        __typeof__(_d) __d = (_d);                                             \
-        COLOR(CLAMP(COLOR_R(_c) + __d, 0, 7), CLAMP(COLOR_G(_c) + __d, 0, 7),  \
-              CLAMP(COLOR_B(_c) + __d, 0, 3));                                 \
-    })
-
 void screen_init(void);
 
 void putpixel(int pos_x, int pos_y, u8 VGA_COLOR);
