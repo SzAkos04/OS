95577ace29900ebdc77835e4fca11a20d092c4fb
SzAkos04 <arisenwall1124@gmail.com>
2024-05-10 19:56:56 +0200

    Fixed colors

--- src/kernel/kernel.c
+++ src/kernel/kernel.c
@@ -5,16 +5,17 @@
 void _main_c() {
     screen_init();
     clear_screen();
-    draw_rect(50, 50, 100, 100, COLOR(0x00, 0x00, 0xff));
 
-    draw_line(50, 50, 75, 75, COLOR(0x00, 0xff, 0x00));
-    draw_line(50, 100, 75, 125, COLOR(0x00, 0xff, 0x00));
-    draw_line(100, 50, 125, 75, COLOR(0xff, 0xff, 0x00));
-    draw_line(100, 100, 125, 125, COLOR(0xff, 0xff, 0x00));
+    draw_rect(50, 50, 100, 100, COLOR(7, 0, 0));
 
-    draw_rect(75, 75, 125, 125, COLOR(0xff, 0x00, 0x00));
+    draw_line(50, 50, 75, 75, COLOR(7, 0, 0));
+    draw_line(50, 100, 75, 125, COLOR(7, 0, 0));
+    draw_line(100, 50, 125, 75, COLOR(7, 0, 0));
+    draw_line(100, 100, 125, 125, COLOR(7, 0, 0));
+
+    /* draw_rect(75, 75, 125, 125, COLOR(7, 0, 0)); */
     // FIXME: for some reason this doesn't print anything, but if I comment out
     // a `draw_line()` function, it works, looks like it has a limit on how much
     // it can put on the screen
-    print_string("CUBE", 75, 30, COLOR(0xff, 0xff, 0xff));
+    print_string("CUBE", 75, 30, COLOR(7, 0, 0));
 }
